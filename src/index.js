2de4483ec9d9b19cfbebc931fd7c5e90caa4c2bc
mae116 <mae116>
2022-12-22 10:18:51 +0800

    databse

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import{getAuth, signInWithPopup, GoogleAuthProvider} from "firebase/auth";
+import{getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut, signInWithEmailAndPassword} from "firebase/auth";
+import{getFirestore, collection, addDoc} from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -35,3 +37,42 @@ googleSignInBtn.addEventListener('click', () => {
     alert(`Error ${errorMessage}`)
   });
 });
+
+onAuthStateChanged(auth, (user) => {
+  if (user){
+    alert(`User has signed in! The user in session is: ${user.email}`)
+  }else{  
+    alert('No user currently')
+  }
+  })
+
+  const signOutBtn = document.querySelector('.sign-out');
+
+  signOutBtn.addEventListener('click', () => {
+    signOut(auth);
+  })
+
+  
+  const epSignInBtn = document.querySelector('.ep-sign-in');
+
+  epSignInBtn.addEventListener('click', () => {
+    const email = 'try@email.com';
+    const pass = 'try123456';
+    signInWithEmailAndPassword(auth, email, pass).then((result) => {
+      alert('Hi, you signed in with an email and password')
+    }).catch((error) => {
+      const errorMessage = error.message;
+      alert(`Error: ${errorMessage}`)
+    });
+  })
+
+const db = getFirestore(app);
+const saveBtn = document.querySelector(".save")
+saveBtn.addEventListener("click",async() => {
+  const collectionRef = collection(db, "desserts")
+  const newDessert = await addDoc (collectionRef , {
+    name: "Leche Plan",
+    price: 120000
+  })
+  console.log(`Created a new dessert: ${newDessert.name}`)
+  })
